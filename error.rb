acb66fe9c5c63ee7922f3fa1e8d2ec5cd7511e2d
Conor Horan-Kates <conor.code@gmail.com>
2013-05-01 19:31:10 -0700

    good progress on SFO->MSY flight, but still a ways to go

--- error.rb
+++ error.rb
@@ -2,17 +2,29 @@ require 'rubygems'
 require 'lib/rouster'
 
 begin
-  badkey = Rouster.new(:sshkey => 'foo', :verbose => 4, :name => 'whatever')
+  badkey = Rouster.new('whatever', 4, __FILE__, 'foo', false)
 rescue Rouster::InternalError => e
   p "caught #{e.class}: #{e.message}"
+rescue => e
+  p "caught unexpected #{e.class}: #{e.message}"
 end
 
 p badkey
 
 begin
-  badvagrantfile = Rouster.new(:vagrantfile => 'foo', :verbose => 4, :name => 'likehesaid')
+  badvagrantfile = Rouster.new('likehesaid', 4, 'foo', __FILE__, false)
 rescue Rouster::InternalError => e
   p "caught #{e.class}: #{e.message}"
+rescue => e
+  p "caught unexpected #{e.class}: #{e.message}"
 end
 
 p badvagrantfile
+
+begin
+  good = Rouster.new('app', 4, nil, '/Users/choran-kates/.vagrant.d/insecure_private_key', false)
+rescue => e
+  p "caught #{e.class}: #{e.message}"
+end
+
+p good
--- lib/rouster.rb
+++ lib/rouster.rb
@@ -1,16 +1,34 @@
 require 'rubygems'
-#require 'vagrant'
+
+# TODO be smarter about this
+#require '/Applications/Vagrant/embedded/gems/gems/vagrant-1.0.5/lib/vagrant'
+
+# TODO combine/refactor the get_scp_command(), get_ssh_command() synergies
+
+# need to install log4r before can do above
+# -- looks like vagrant/cli will be one that we want -> env = Vagrant::Environment.new({}); env.cli(ARGV)
+# -- and vagrant/ssh will be the other -> ssh = Vagrant::SSH.new(vm) <-- need to see if that is actually correct
 
 class Rouster
 
-  attr_reader :name, :passthrough, :sshkey, :vagrant, :vagrantdir, :vagrantfile
-  attr_accessor :verbosity
+  attr_reader :name, :output, :passthrough, :sshkey, :sudo, :vagrant, :vagrantdir, :vagrantfile
+  attr_accessor :sshinfo, :verbosity
 
-  def initialize (name = 'unknown', verbosity = 0, vagrantfile = nil, sshkey = nil, passthrough = false)
+  # poor man logging values for now
+  NOTICE = 0
+  ERROR  = 1
+  WARN   = 2
+  INFO   = 3
+  DEBUG  = 4
+
+
+  def initialize (name = 'unknown', verbosity = 0, vagrantfile = nil, sshkey = nil, sudo = true, passthrough = false)
     @name        = name
+    @output      = Array.new
     @passthrough = passthrough
     @sshkey      = sshkey
-    @vagrantfile = vagrantfile.nil? ? sprintf('%s/Vagrantfile', Dir.pwd) : vagrantfile # doing it here so the instantiation line isn't too long
+    @sudo        = (sudo.nil? or @passthrough.eql?(true)) ? false : true
+    @vagrantfile = vagrantfile.nil? ? sprintf('%s/Vagrantfile', Dir.pwd) : vagrantfile
     @vagrantdir  = File.dirname(@vagrantfile)
     @verbosity   = verbosity
 
@@ -36,6 +54,10 @@ class Rouster
     unless File.exists?(@vagrantfile)
       raise Rouster::InternalError, "specified Vagrantfile [#{@vagrantfile}] does not exist"
     end
+
+    # TODO need to confirm validity before we go on
+    # in Salesforce::Vagrant we constantly checked whether an object was 'valid' and then again at its 'status' -- not doing that again
+
 
     # instantiate a Vagrant worker (or 2 or 3) here
     # need to run commands over ssh tunnel, sned files, get files
@@ -71,6 +93,8 @@ class Rouster
   ## internal methods
   def run(command)
     # runs a command inside the Vagrant VM
+    cmd     = sprintf('%s -t %s%s', self.get_ssh_prefix(), self.uses_sudo? ? 'sudo ' : '', command)
+    self._run(cmd)
   end
 
   def run_vagrant(command)
@@ -91,14 +115,9 @@ class Rouster
 
   def available_via_ssh?
     # functional test to see if Vagrant machine can be logged into via ssh
-    # ssh and run uname or something similarly harmless and test exit code (? or use something internal to vagrant?)
-
-    # refactor this later
-    cmd_prefix = self.get_ssh_command()
-    cmd = '%s -t uname -a' % cmd_prefix
 
     begin
-      self._run(cmd)
+      self.run('uname -a')
     rescue Rouster::SSHConnectionError
       false
     end
@@ -107,81 +126,191 @@ class Rouster
   end
 
   def log(msg, level=NOTICE)
+    #raise Rouster::NotImplementedError
+    puts "#{level}: #{msg}\n"
+  end
+
+  def get(remote_file, local_file=nil)
+    local_file = local_file.nil? ? File.basename(remote_file) : local_file
+
+    res = self.status()
+
+    raise Rouster::SSHConnectionError sprintf('unable to get [%s], box is in status [%s]', remote_file, res) unless res.eql?('running')
+
+    cmd = sprintf(
+      '%s %s@%s:%s %s',
+      self.get_scp_prefix(),
+      self.sshinfo[:user],
+      self.sshinfo[:hostname],
+      remote_file,
+      local_file
+    )
+
+    begin
+      # assuming this doesn't fail, does the return of this method get passed back up the stack?
+      self._run(cmd)
+    rescue Rouster::LocalExecutionError => e
+      raise Rouster::SSHConnectionError sprintf('unable to get [%s], command [%s] returned [%s]', remote_file, cmd, self.get_output())
+    end
+
+  end
+
+  def put(local_file, remote_file=nil)
+    remote_file = remote_file.nil? ? File.basename(local_file) : remote_file
+
+    res = self.status()
+    raise Rouster::SSHConnectionError sprintf('unable to get [%s], box is in status [5s]', local_file, res) unless res.eql?('running')
+
+    cmd = sprintf(
+      '%s %s %s@%s:%s',
+      self.get_scp_prefix(),
+      local_file,
+      self.sshinfo[:user],
+      self.sshinfo[:hostname],
+      remote_file
+    )
+
+    begin
+      self._run(cmd)
+    rescue Rouster::LocalExecutionError
+      raise Rouster::SSHConnectionError sprintf('unable to get [%s], command [%s] returned [%s]', local_file, cmd, self.get_output())
+    end
 
   end
 
   # there has _got_ to be a more rubyish way to do this
   def is_passthrough?
-    self.passthrough.eql?(false) ? false : true
+    self.passthrough.eql?(true)
   end
 
-  def rebuild(wait = 120)
-    # destroys/reups a Vagrant machine, wait time is how long to wait for it to be available_via_ssh before throwing an exception
+  def uses_sudo?
+    # convenience method for the @sudo attribute
+     self.sudo.eql?(true)
+  end
+
+  def rebuild()
+    # destroys/reups a Vagrant machine
+
+    self.run_vagrant('destroy')
+    self.run_vagrant('up')
+
+    # TODO decide what the return from this should be.. nothing? only throw exceptions when you need to?
+
   end
 
   def restart(wait = 120)
     # restarts a Vagrant machine, wait time is same as rebuild()
     # how do we do this in a generic way? shutdown -rf works for Unix, but not Solaris
+
+    # MVP
+    self.run('/sbin/shutdown -rf now')
+
+    # TODO implement some 'darwin award' checks in case someone tries to reboot a local passthrough?
+
   end
 
   def _run(command)
     # shells out and executes a command locally on the system, different than run(), which operates in the VM
     # returns STDOUT|STDERR, raises Rouster::LocalExecutionError on non 0 exit code
 
-    cmd    = sprintf('%s', command)
-    output = `#{cmd}`
+    tmp_file = sprintf('/tmp/rouster.%s.%s', Time.now.to_i, $$)
+    cmd      = sprintf('%s > %s 2> %s', command, tmp_file, tmp_file)
+    res      = `#{cmd}` # what does this actually hold?
+
+    output = File.read(tmp_file)
+    File.delete(tmp_file) or raise Rouster::InternalError sprintf('unable to delete [%s]: %s', tmp_file, $!)
 
     unless $?.success?
-      raise Rouster::LocalExecutionError 'command [%s] exited with [%s]' % cmd, $?.to_i()
+      raise Rouster::LocalExecutionError sprintf('command [%s] exited with code [%s], output [%s]', cmd, $?.to_i(), output)
     end
 
+    self.output.push(output)
     output
-
   end
 
   ## truly internal methods
-  def get_ssh_command
-    output = self.run_vagrant("ssh-config #{self.name}")
-    hash   = Hash.new
+  def get_ssh_prefix
+    # TODO replace this with something Vagranty
 
-    output.each_line do |line|
-      if line =~ /HostName (.*?)$/
-        hash[:hostname] = $1
-      elsif line =~ /User (\w*?)$/
-        hash[:user] = $1
-      elsif line =~ /Port (\d+)$/
-        hash[:port] = $1
-      elsif line =~ /IdentityFile (.*?)$/
-        hash[:sshkey] = $1
+    if self.sshinfo.nil?
+      hash   = Hash.new
+      output = self.run_vagrant("ssh-config #{self.name}")
+
+      output.each_line do |line|
+        if line =~ /HostName (.*?)$/
+          hash[:hostname] = $1
+        elsif line =~ /User (\w*?)$/
+          hash[:user] = $1
+        elsif line =~ /Port (\d*?)$/
+          hash[:port] = $1
+        elsif line =~ /IdentityFile (.*?)$/
+          hash[:sshkey] = $1
+        end
       end
+
+      self.sshinfo = hash
     end
 
     sprintf(
-        'ssh -p %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=Error -o IdentitiesOnly=yes -i %s %s@%s',
-        hash[:port],
-        hash[:sshkey],
-        hash[:user],
-        hash[:hostname]
+      'ssh -p %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=Error -o IdentitiesOnly=yes -i %s %s@%s',
+      self.sshinfo[:port],
+      self.sshinfo[:sshkey],
+      self.sshinfo[:user],
+      self.sshinfo[:hostname]
     )
   end
 
-  def output(index = 0)
+  def get_scp_prefix
+
+    if self.sshinfo.nil?
+      hash   = Hash.new
+      output = self.run_vagrant("ssh-config #{self.name}")
+
+      output.each_line do |line|
+        if line =~ /HostName (.*?)$/
+          hash[:hostname] = $1
+        elsif line =~ /User (\w*?)$/
+          hash[:user] = $1
+        elsif line =~ /Port (\d*?)$/
+          hash[:port] = $1
+        elsif line =~ /IdentityFile (.*?)$/
+          hash[:sshkey] = $1
+        end
+      end
+
+      self.sshinfo = hash
+    end
+
+    a = sprintf(
+      'scp -B -P %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=Error -o IdentitiesOnly=yes -i %s',
+      self.sshinfo[:port],
+      self.sshinfo[:sshkey]
+    )
+
+    a
+  end
+
+  def get_output(index = 0)
     # return index'th array of output in LIFO order
+
+    # TODO do this in a mathy way instead of a youre-going-to-run-out-of-memory-way
+    reversed = self.output.reverse
+    reversed[index]
   end
 
 end
 
-# custom exceptions -- what else do we want them to include/do?
-class Rouster::InternalError < StandardError
-  # thrown by most (if not all) Rouster methods
+# custom exceptions -- what else do we want them to include/do?   should append the name of the box to the exception message
+class Rouster::NotImplementedError < StandardError
+  # could have sworn there was a built in 'not implemented' exception.. guess this'll do just as well
 end
 
 class Rouster::FileTransferError < StandardError
   # thrown by get() and put()
 end
 
-class Rouster::SSHConnectionError < StandardError
-  # thrown by available_via_ssh() -- and potentially _run()
+class Rouster::InternalError < StandardError
+  # thrown by most (if not all) Rouster methods
 end
 
 class Rouster::LocalExecutionError < StandardError
@@ -190,4 +319,8 @@ end
 
 class Rouster::RemoteExecutionError < StandardError
   # thrown by run()
+end
+
+class Rouster::SSHConnectionError < StandardError
+  # thrown by available_via_ssh() -- and potentially _run()
 end
