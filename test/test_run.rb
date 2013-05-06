c8d806e7aae6c630c60b041fe5793857f7ead4e7
Conor Horan-Kates <conor.code@gmail.com>
2013-05-06 12:32:35 -0600

    fleshed out some more tests for run()

--- test/test_run.rb
+++ test/test_run.rb
@@ -7,26 +7,48 @@ class TestRun < Test::Unit::TestCase
 
   def setup
     @app = Rouster.new({:name => 'app'})
+    @acs = Rouster.new({:name => 'acs', :sudo => false})
+
+    @app.up()
+    @acs.up()
   end
 
-  def test_it
+  def test_happy_path
+    res = @app.run('ls -l')
 
-    workers = [@app]
+    assert_equal(0, @app.exitcode, 'got expected exit code')
+    assert_equal(res, @app.get_output(), 'return matches get_output()')
+    assert_not_nil(@app.get_output(), 'output is populated')
+    assert_match(/^total\s\d/, @app.get_output(), 'output matches expectations')
+  end
 
-    workers.each do |w|
-      # tear them down and build back up for clean run
-      w.destroy()
-      w.up()
+  def test_bad_exit_codes
+    res = @app.run('fizzbang')
 
-      res = w.run('puppet agent -t --environment development')
-      assert_equal(w.exitcode.eql?(0) or w.exitcode.eql?(2), w.exitcode, "exit code [#{w.exitcode}] considered success")
-      assert_match(/Finished catalog/, res, "output contains 'Finished catalog'")
+    assert_not_equal(0, @app.exitcode, 'got expected non-0 exit code')
+    assert_equal(res, @app.get_output(), 'return matches get_output()')
+    assert_not_nil(@app.get_output(), 'output is populated')
+    assert_match(/fizzbang/, @app.get_output(), 'output matches expectations')
+  end
 
-    end
+  def test_sudo_enabled
+    res = @app.run('ls -l /root')
+
+    assert_equal(0, @app.exitcode, 'got expected exit code')
+    assert_no_match(/Permission denied/i, @app.get_output(), 'output matches expectations 1of2')
+    assert_match(/^total\s\d/, @app.get_output(), 'output matches expectations 2of2')
+  end
+
+  def test_sudo_disabled
+    res = @acs.run('ls -l /root')
+
+    assert_not_equal(0, @acs.exitcode, 'got expected non-0 exit code')
+    assert_match(/Permission denied/i, @acs.get_output(), 'output matches expectations')
   end
 
   def teardown
-    @ppm.destroy()
+    # TODO we should suspend if any test failed for triage
+    @app.destroy()
+    @acs.destroy()
   end
-
 end
