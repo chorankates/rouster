f0f6522b0d17ae7772a3b167dd95d4eddc669c6f
Conor Horan-Kates <conor.code@gmail.com>
2013-05-06 12:30:37 -0600

    adding some more stubs to plugins - run_puppet, all the is_* methods from Test::More::piab

--- plugins/puppet.rb
+++ plugins/puppet.rb
@@ -3,15 +3,25 @@ require '../path_helper'
 class Puppet
   include 'rouster'
 
-  def compile_catalog
+  def compile_catalog(hostname)
+    # TODO determine what our inputs should be
+    # TODO determine what/how to call puppet to do this
     raise NotImplementedError.new()
   end
 
-  def get_puppet_errors(input)
+  def run_puppet
+    self.run('/sbin/service puppet once -t')
+  end
+
+  def get_puppet_errors(input = nil)
+    str = input.nil? ? self.get_output() : input
+
     raise NotImplementedError.new()
   end
 
-  def get_puppet_notices(input)
+  def get_puppet_notices(input = nil)
+    str = input.nil? ? self.get_output() : input
+
     raise NotImplementedError.new()
   end
 
