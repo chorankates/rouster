c354d69db1d6d296b8478c2b77a4cecfb09bd0f4
Conor Horan-Kates <conor.code@gmail.com>
2013-05-05 23:51:11 -0600

    removing a completed TODO

--- path_helper.rb
+++ path_helper.rb
@@ -1,11 +1,10 @@
 # heavily influenced by https://github.com/puppetlabs/hiera/blob/master/spec/spec_helper.rb
 
-# TODO should rename this to path_helper, since it's not really for spec
-
 # this gets us Rouster, still need to figure out how to find vagrant
 $LOAD_PATH << File.join([File.dirname(__FILE__), "lib"])
 
 require 'rubygems'
+
 ## this is really optional, so don't die if we don't have it
 begin
   require 'ruby-debug'
