606390fa164b69bc8193f0fe1198fb62cf9b9c6e
Conor Horan-Kates <conor.code@gmail.com>
2013-05-12 17:03:40 -0600

    implementing get_puppet_errors() and get_puppet_notices()

--- lib/rouster/puppet.rb
+++ lib/rouster/puppet.rb
@@ -3,25 +3,27 @@ require sprintf('%s/../../%s', File.dirname(File.expand_path(__FILE__)), 'path_h
 class Rouster
 
     def compile_catalog(hostname)
-      # TODO determine what our inputs should be
       # TODO determine what/how to call puppet to do this
       raise NotImplementedError.new()
     end
 
     def run_puppet
+      # TODO how can we make this more flexible?
       self.run('/sbin/service puppet once -t')
     end
 
     def get_puppet_errors(input = nil)
-      str = input.nil? ? self.get_output() : input
+      str    = input.nil? ? self.get_output() : input
+      errors = str.scan(/35merr:.*/)
 
-      raise NotImplementedError.new()
+      errors.empty? ? nil : errors
     end
 
     def get_puppet_notices(input = nil)
-      str = input.nil? ? self.get_output() : input
+      str     = input.nil? ? self.get_output() : input
+      notices = str.scan(/36mnotice:.*/)
 
-      raise NotImplementedError.new()
+      notices.empty? ? nil : notices
     end
 
 end
