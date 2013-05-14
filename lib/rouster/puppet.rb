4e33f9928d7e25b8e1989ec481f26f0d335076e6
Conor Horan-Kates <conor.code@gmail.com>
2013-05-13 21:09:09 -0600

    initial implementation of facter(), allows specification of whether or not to get custom facts, defaults to true. added .true? and .false? to Object

--- lib/rouster/puppet.rb
+++ lib/rouster/puppet.rb
@@ -3,7 +3,32 @@ require sprintf('%s/../../%s', File.dirname(File.expand_path(__FILE__)), 'path_h
 require 'json'
 require 'socket'
 
+# convenience truthiness methods -- probably belongs somewhere else, only used here so far
+class Object
+  def false?
+    self.eql?(false)
+  end
+
+  def true?
+    self.eql?(true)
+  end
+end
+
+
 class Rouster
+
+  def facter(custom_facts=true)
+    json = nil
+    res  = self.run(sprintf('facter %s', custom_facts.true? ? '-p' : ''))
+
+    begin
+      json = res.to_json
+    rescue
+      raise InternalError.new(sprintf('unable to parse[%s] as JSON', res))
+    end
+
+    json
+  end
 
   # TODO we should be able to run this without upping the box in question --
   # just need to be able to talk to the same puppetmaster, which means we should 'require puppet'
@@ -23,11 +48,6 @@ class Rouster
     json
   end
 
-  def run_puppet
-    # TODO should we make this more flexible?
-    self.run('/sbin/service puppet once -t')
-  end
-
   def get_puppet_errors(input = nil)
     str    = input.nil? ? self.get_output() : input
     errors = str.scan(/35merr:.*/)
@@ -41,5 +61,11 @@ class Rouster
 
     notices.empty? ? nil : notices
   end
+
+  def run_puppet
+    # TODO should we make this more flexible?
+    self.run('/sbin/service puppet once -t')
+  end
+
 
 end
