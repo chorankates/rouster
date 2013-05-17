2324656d0869e6e5d3cf0f1b050a6f68094bc757
Conor Horan-Kates <conor.code@gmail.com>
2013-05-17 09:18:46 -0600

    cleaning up test_generate_unique_mac

--- test/unit/test_generate_unique_mac.rb
+++ test/unit/test_generate_unique_mac.rb
@@ -6,7 +6,9 @@ require 'test/unit'
 class TestPut < Test::Unit::TestCase
 
   def setup
-    @app = Rouster.new(:name => 'app')
+    assert_nothing_raised do
+      @app = Rouster.new(:name => 'app')
+    end
   end
 
   def test_happy_path
@@ -18,10 +20,13 @@ class TestPut < Test::Unit::TestCase
   end
 
   def test_uniqueness
-    a = @app.generate_unique_mac
-    b = @app.generate_unique_mac
 
-    assert_not_equal(a, b)
+    (0..100).each do |i|
+      a = @app.generate_unique_mac
+      b = @app.generate_unique_mac
+
+      assert_not_equal(a, b)
+    end
 
   end
 
