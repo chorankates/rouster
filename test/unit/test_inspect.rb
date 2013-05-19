5e4b627fb9c4ca6046b51aea9f951ed681229c7b
Conor Horan-Kates <conor.code@gmail.com>
2013-05-19 09:59:58 -0600

    super MVP implementatin of inspect(), #13

--- test/unit/test_inspect.rb
+++ test/unit/test_inspect.rb
@@ -12,7 +12,17 @@ class TestPut < Test::Unit::TestCase
   end
 
   def test_happy_path
-    # want to do this with 'assert_method_called' or something similar
+    # want to do this with 'assert_method_called' or something similar -- but for now..
+
+    res = @app.inspect()
+
+    assert_not_nil(res.match(/created/))
+    assert_not_nil(res.match(/passthrough\[false\]/))
+    assert_not_nil(res.match(/sshkey/))
+    assert_not_nil(res.match(/status/))
+    assert_not_nil(res.match(/sudo\[true\]/))
+    assert_not_nil(res.match(/vagrantfile/))
+    assert_not_nil(res.match(/verbosity\[5\]/))
   end
 
   def teardown
