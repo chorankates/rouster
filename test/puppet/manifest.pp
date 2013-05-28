8eeb982fa03718567398e968567a9bbaba12c56f
Conor Horan-Kates <conor.code@gmail.com>
2013-05-28 15:23:41 -0700

    basic puppet manifest to support some TDD

---
+++ test/puppet/manifest.pp
@@ -0,0 +1,57 @@
+# == manifest.pp
+
+node default {
+  include baseclass
+}
+
+node 'app' {
+  include app_role
+}
+
+node 'db' {
+  include db_role
+}
+
+class baseclass {
+
+  file { '/etc/passwd':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  file { '/tmp/foo':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  package { ['puppet', 'facter'] :
+    ensure => installed,
+  }
+
+}
+
+class app_role {
+
+package { 'rsync':
+    ensure => installed,
+  }
+
+}
+
+class db_role {
+
+  package { 'httpd':
+    ensure => installed,
+  }
+
+  file { '/tmp/fizzy':
+    ensure   => file,
+    contents => 'this is a test',
+    owner    => 'vagrant',
+    group    => 'vagrant',
+    mode     => '0444',
+  }
+}
