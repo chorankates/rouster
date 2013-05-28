d6ced3bd87c59ce3d4f621ba874e9b24c39959dd
Conor Horan-Kates <conor.code@gmail.com>
2013-05-27 17:31:07 -0700

    MVP - failing some tests already, look legitimate

--- test/unit/test_parse_ls_string.rb
+++ test/unit/test_parse_ls_string.rb
@@ -3,6 +3,7 @@ require sprintf('%s/../../path_helper', File.dirname(File.expand_path(__FILE__))
 # this is based on RHEL output, how cross compatible is this?
 
 require 'rouster'
+require 'rouster/tests'
 require 'test/unit'
 
 class TestPut < Test::Unit::TestCase
@@ -14,79 +15,19 @@ class TestPut < Test::Unit::TestCase
   end
 
   def test_readable_by_all
-    str = ''
+    str = "-r--r--r-- 1 root root 199 May 27 22:51 /readable\n"
 
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_readable_by_u
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_readable_by_g
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_readable_by_o
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_writable_by_all
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_writable_by_u
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_writable_by_g
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_writable_by_o
-    str = ''
-
-    expectation = { }
+    expectation = {
+      :directory?  => false,
+      :file?       => true,
+      :mode        => '0444',
+      :owner       => 'root',
+      :group       => 'root',
+      :size        => '199',
+      :executable? => [false, false, false],
+      :readable?   => [true, true, true],
+      :writeable?  => [false, false, false]
+    }
 
     res = @app.parse_ls_string(str)
 
@@ -94,48 +35,57 @@ class TestPut < Test::Unit::TestCase
   end
 
   def test_executable_by_all
-    str = ''
+    str = "-rwxrwxrwx 1 root root 199 May 27 22:51 /executable\n"
 
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_executable_by_u
-    str = ''
-
-    expectation = { }
-
-    res = @app.parse_ls_string(str)
-
-    assert_equal(expectation, res)
-  end
-
-  def test_executable_by_g
-    str = ''
-
-    expectation = { }
+    expectation = {
+        :directory?  => false,
+        :file?       => true,
+        :mode        => '0777',
+        :owner       => 'root',
+        :group       => 'root',
+        :size        => '199',
+        :executable? => [true, true, true],
+        :readable?   => [true, true, true],
+        :writeable?  => [true, true, true]
+    }
 
     res = @app.parse_ls_string(str)
 
     assert_equal(expectation, res)
   end
 
-  def test_executable_by_o
-    str = ''
+  def test_writable_by_all
+    str = "-r--r--r-- 1 root root 199 May 27 22:51 /readable\n"
 
-    expectation = { }
+    expectation = {
+        :directory?  => false,
+        :file?       => true,
+        :mode        => '0666',
+        :owner       => 'root',
+        :group       => 'root',
+        :size        => '199',
+        :executable? => [false, false, false],
+        :readable?   => [true, true, true],
+        :writeable?  => [true, true, true]
+    }
 
     res = @app.parse_ls_string(str)
 
     assert_equal(expectation, res)
   end
 
+  def test_dir_detection
+    dir_str = "drwxrwxrwt 5 root root 4096 May 28 00:26 /tmp/\n"
+    file_str  = "-rw-r--r-- 1 root    root      906 Oct  2  2012 grub.conf\n"
 
+    dir  = @app.parse_ls_string(dir_str)
+    file = @app.parse_ls_string(file_str)
 
-  def test_errors
+    assert_equal(true,  dir[:directory?])
+    assert_equal(false, dir[:file?])
+
+    assert_equal(false, file[:directory?])
+    assert_equal(true,  file[:file?])
 
   end
 
