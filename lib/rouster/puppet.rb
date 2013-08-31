d0c623e48c285150da61d9babf77be886cc7d04d
Conor Horan-Kates <choran-kates@salesforce.com>
2013-08-31 12:28:22 -0700

    initial RDoc

--- lib/rouster/puppet.rb
+++ lib/rouster/puppet.rb
@@ -5,8 +5,18 @@ require 'net/https'
 require 'socket'
 require 'uri'
 
+# TODO use @cache_timeout to invalidate data cached here
+
 class Rouster
 
+  ##
+  # facter
+  #
+  # runs facter, returns parsed hash of { fact1 => value1, factN => valueN }
+  #
+  # parameters
+  # [cache] - whether to store/return cached facter data, if available
+  # [custom_facts] - whether to include custom facts in return (uses -p argument)
   def facter(cache=true, custom_facts=true)
     if cache.true? and ! self.facts.nil?
       self.facts
@@ -27,12 +37,25 @@ class Rouster
     res
   end
 
+  ##
+  # get_catalog
+  #
+  # not completely implemented method to get a compiled catalog about a node (based on its facts) from a puppetmaster
+  #
+  # original implementation used the catalog face, which does not actually work. switched to an API call, but still need to convert facts into PSON
+  #
+  # parameters
+  # [hostname] - hostname of node to return catalog for, if not specified, will use `hostname --fqdn`
+  # [puppetmaster] - hostname of puppetmaster to use in API call, defaults to 'puppet'
+  # [facts] - hash of facts to pass to puppetmaster
+  # [puppetmaster_port] - port to talk to the puppetmaster on, defaults to 8140
   def get_catalog(hostname=nil, puppetmaster=nil, facts=nil, puppetmaster_port=8140)
     # post https://<puppetmaster>/catalog/<node>?facts_format=pson&facts=<pson URL encoded> == ht to patrick@puppetlabs
     certname     = hostname.nil? ? self.run('hostname --fqdn').chomp : hostname
     puppetmaster = puppetmaster.nil? ? 'puppet' : puppetmaster
-    facts        = facts.nil? ? self.facter() : facts # TODO check for presence of certain 'required' facts?
+    facts        = facts.nil? ? self.facter() : facts # TODO check for presence of certain 'required' facts/datatype?
 
+    raise InternalError.new('need to finish conversion of facts to PSON')
     facts.to_pson # this does not work, but needs to
 
     json = nil
@@ -49,6 +72,13 @@ class Rouster
     json
   end
 
+  ##
+  # get_puppet_errors
+  #
+  # parses input for puppet errors, returns array of strings
+  #
+  # parameters
+  # [input] - string to look at, defaults to self.get_output()
   def get_puppet_errors(input=nil)
     str    = input.nil? ? self.get_output() : input
     errors = str.scan(/35merr:.*/)
@@ -56,6 +86,13 @@ class Rouster
     errors.empty? ? nil : errors
   end
 
+  ##
+  # get_puppet_notices
+  #
+  # parses input for puppet notices, returns array of strings
+  #
+  # parameters
+  # [input] - string to look at, defaults to self.get_output()
   def get_puppet_notices(input=nil)
     str     = input.nil? ? self.get_output() : input
     notices = str.scan(/36mnotice:.*/)
@@ -63,6 +100,10 @@ class Rouster
     notices.empty? ? nil : notices
   end
 
+  ##
+  # get_puppet_version
+  #
+  # executes `puppet --version` and returns parsed version string or nil
   def get_puppet_version
     version   = nil
     installed = self.is_in_path?('puppet')
@@ -77,18 +118,35 @@ class Rouster
     version
   end
 
+  ##
+  # hiera
+  #
+  # returns hiera results from self
+  #
+  # parameters
+  # <key> - hiera key to look up
+  # [config] - path to hiera configuration -- this is only optional if you have a hiera.yaml file in ~/vagrant
   def hiera(key, config=nil)
 
+    # TODO implement this
     raise NotImplementedError.new()
 
   end
 
+  ##
+  # parse_catalog
+  #
+  # looks at the ['data']['resources'] keys in catalog for Files, Groups, Packages, Services and Users, returns hash of expectations compatible with validate_*
+  #
+  # this is a very lightly tested implementation, please open issues as necessary
+  #
+  # parameters
+  # <catalog> - JSON string or Hash representation of catalog, typically from get_catalog()
   def parse_catalog(catalog)
     classes   = nil
     resources = nil
     results   = Hash.new()
 
-    # support either JSON or already parsed Hash
     if catalog.is_a?(String)
       begin
         JSON.parse!(catalog)
@@ -188,9 +246,15 @@ class Rouster
     results
   end
 
+  ##
+  # remove_existing_certs
+  #
+  # ... removes existing certificates - really only useful when called on a puppetmaster
+  # useful in testing environments where you want to destroy/rebuild agents without rebuilding the puppetmaster every time (think autosign)
+  #
+  # parameters
+  # <puppetmaster> - string/partial regex of certificate names to keep
   def remove_existing_certs (puppetmaster)
-    # removes all certificates that a puppetmaster knows about aside from it's own (useful in testing where autosign is in use)
-    # really only useful if called from a puppet master
     hosts = Array.new()
 
     res = self.run('puppet cert list --all')
@@ -208,6 +272,26 @@ class Rouster
 
   end
 
+  ##
+  # run_puppet
+  #
+  # ... runs puppet on self, returns nothing
+  #
+  # currently supports 2 methods of running puppet:
+  #  - master - runs '/sbin/service puppet once -t'
+  #    - supported options
+  #      - expected_exitcode - string/integer/array of acceptable exit code(s)
+  #  - masterless - runs 'puppet apply <options>' after determining version of puppet running and adjusting arguments
+  #    - supported options
+  #      - expected_exitcode - string/integer/array of acceptable exit code(s)
+  #      - hiera_config - path to hiera configuration -- only supported by Puppet 3.0+
+  #      - manifest_file - string/array of strings of paths to manifest(s) to apply
+  #      - manifest_dir - string/array of strings of directories containing manifest(s) to apply - is recursive
+  #      - module_dir - path to module directory -- currently a required parameter, is this correct?
+  #
+  # parameters
+  # [mode] - method to run puppet, defaults to 'master'
+  # [opts] - hash of additional options
   def run_puppet(mode='master', passed_opts=nil)
 
     if mode.eql?('master')
