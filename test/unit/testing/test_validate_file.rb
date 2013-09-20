4ce8fd3aba72f2dfc95eb65e470d58d6afeb4d16
Conor Horan-Kates <conor.code@gmail.com>
2013-09-20 14:30:11 -0700

    updating unit tests

--- test/unit/testing/test_validate_file.rb
+++ test/unit/testing/test_validate_file.rb
@@ -44,37 +44,40 @@ class TestValidateFile < Test::Unit::TestCase
     @app = Rouster.new(:name => 'app', :unittest => true)
     @app.deltas[:files] = fake_files
     @app.facts = fake_facts
+
+    # not quite a unit test,.
+    skip unless @app.status().eql?('running')
   end
 
   def test_positive_basic
 
-    assert(@app.validate_file('/etc/hosts', { :ensure => 'file', :mode => '0644', :file => true, :directory => false,  :owner => 'root', :group => 'root' }, true))
-    assert(@app.validate_file('/etc/hosts', { :exists => 'present' }, true ))
-    assert(@app.validate_file('/etc/hosts', { }, true ))
-    assert(@app.validate_file('/etc/hosts', { :mode => '0644' }, true))
-    assert(@app.validate_file('/etc/hosts', { :permissions => '0644' }, true))
-    assert(@app.validate_file('/etc/hosts', { :size => 166 }, true ))
-    assert(@app.validate_file('/etc/hosts', { :size => '166' }, true ))
-    assert(@app.validate_file('/etc/hosts', { :file => 'true' }, true))
-    assert(@app.validate_file('/etc/hosts', { :file => true }, true))
-    assert(@app.validate_file('/etc/hosts', { :directory => 'false' }, true))
-    assert(@app.validate_file('/etc/hosts', { :directory => false }, true))
-    assert(@app.validate_file('/etc/hosts', { :owner => 'root' }, true))
-    assert(@app.validate_file('/etc/hosts', { :group => 'root' }, true))
+    assert(@app.validate_file('/etc/hosts', { :ensure => 'file', :mode => '0644', :file => true, :directory => false,  :owner => 'root', :group => 'root' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :exists => 'present' }, false, true ))
+    assert(@app.validate_file('/etc/hosts', { }, false, true ))
+    assert(@app.validate_file('/etc/hosts', { :mode => '0644' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :permissions => '0644' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :size => 166 }, false, true ))
+    assert(@app.validate_file('/etc/hosts', { :size => '166' }, false, true ))
+    assert(@app.validate_file('/etc/hosts', { :file => 'true' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :file => true }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :directory => 'false' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :directory => false }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :owner => 'root' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :group => 'root' }, false, true))
 
-    assert(@app.validate_file('/etc/fizzbang', { :ensure => 'absent' }, true))
-    assert(@app.validate_file('/etc/fizzbang', { :ensure => false }, true ))
+    assert(@app.validate_file('/etc/fizzbang', { :ensure => 'absent' }, false, true))
+    assert(@app.validate_file('/etc/fizzbang', { :ensure => false }, false, true ))
 
-    assert(@app.validate_file('/etc', { :ensure => 'directory' }, true))
-    assert(@app.validate_file('/etc', { :ensure => 'dir' }, true))
-    assert(@app.validate_file('/etc', { :ensure => 'dir', :file => 'false' }, true))
-    assert(@app.validate_file('/etc', { :ensure => 'dir', :directory => 'true' }, true))
-    assert(@app.validate_file('/etc', { :ensure => 'dir', :file => 'false', :directory => 'true' }, true))
+    assert(@app.validate_file('/etc', { :ensure => 'directory' }, false, true))
+    assert(@app.validate_file('/etc', { :ensure => 'dir' }, false, true))
+    assert(@app.validate_file('/etc', { :ensure => 'dir', :file => 'false' }, false, true))
+    assert(@app.validate_file('/etc', { :ensure => 'dir', :directory => 'true' }, false, true))
+    assert(@app.validate_file('/etc', { :ensure => 'dir', :file => 'false', :directory => 'true' }, false, true))
 
-    assert(@app.validate_file('/fizzy', { :ensure => 'absent' }, true))
-    assert(@app.validate_file('/fizzy', { :ensure => false }, true))
-    assert(@app.validate_file('/fizzy', { :exists => 'false' }, true))
-    assert(@app.validate_file('/fizzy', { :exists => false }, true))
+    assert(@app.validate_file('/fizzy', { :ensure => 'absent' }, false, true))
+    assert(@app.validate_file('/fizzy', { :ensure => false }, false, true))
+    assert(@app.validate_file('/fizzy', { :exists => 'false' }, false, true))
+    assert(@app.validate_file('/fizzy', { :exists => false }, false, true))
 
     # TODO need to do :contains testing in a non-unit context
 
@@ -82,35 +85,36 @@ class TestValidateFile < Test::Unit::TestCase
 
   def test_positive_constrained
 
-    assert(@app.validate_file('/etc/hosts', { :mode => '0644', :constrain => 'is_virtual true' }, true))
+    assert(@app.validate_file('/etc/hosts', { :mode => '0644', :constrain => 'is_virtual true' }, false, true))
 
   end
 
   def test_negative_basic
 
-    assert_equal(false, @app.validate_file('/etc/hosts', { :mode => '0777' }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :permissions => '0777' }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :file => 'false' }, true ))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :file => false }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :directory => 'true' }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :directory => true }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :size => 'foo' }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :size => 100 }, true))
-    assert_equal(false, @app.validate_file('/etc/hosts', { :size => '100'}, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :mode => '0777' }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :permissions => '0777' }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :file => 'false' }, false, true ))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :file => false }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :directory => 'true' }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :directory => true }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :size => 'foo' }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :size => 100 }, false, true))
+    assert_equal(false, @app.validate_file('/etc/hosts', { :size => '100'}, false, true))
 
     # TODO figure out how to run these in a truly unit-y way, since the data is not present in faked hash, we will fall through to functional testing
-    assert_equal(false, @app.validate_file('/foo', {}, true))
-    assert_equal(false, @app.validate_file('/fizzy', { :ensure => 'directory' }, true))
-    assert_equal(false, @app.validate_file('/bang', { :ensure => 'file' }, true))
+    assert_equal(false, @app.validate_file('/foo', {}, false, true))
+    assert_equal(false, @app.validate_file('/fizzy', { :ensure => 'directory' }, false, true))
+    assert_equal(false, @app.validate_file('/bang', { :ensure => 'file' }, false, true))
 
   end
 
   def test_negative_constrained
 
-    assert(@app.validate_file('/etc/hosts', { :mode => '0644', :constrain => 'is_virtual false' }, true))
-    assert(@app.validate_file('/etc/hosts', { :mode => '0999', :constrain => 'is_virtual false' }, true))
+    assert(@app.validate_file('/etc/hosts', { :mode => '0644', :constrain => 'is_virtual false' }, false, true))
+    assert(@app.validate_file('/etc/hosts', { :mode => '0999', :constrain => 'is_virtual false' }, false, true))
 
   end
+
 
   def teardown
     # noop
